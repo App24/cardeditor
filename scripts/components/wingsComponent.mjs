935f68e8561375c90aabc9df1775338fe2d1a399
Ricardo <rmmx03@gmail.com>
2022-06-09 08:16:18 +0100

    Different scale for wings

--- scripts/components/wingsComponent.mjs
+++ scripts/components/wingsComponent.mjs
@@ -12,11 +12,11 @@ export class WingsComponent extends Component {
         super("Wings", "wings");
         this.subComponents = [
             new PositionComponent(600, 260),
-            new ScaleComponent()
+            new ScaleComponent("Primary Wings Scale", "wingsAScale"),
+            new ScaleComponent("Secondary Wings Scale", "wingsBScale")
         ];
         this.dataTypes = [
             new CheckboxDataType("Follow PFP Image", "followPfp", true, () => this.togglePosition()),
-            new CheckboxDataType("Auto Resize", "autoSize", true, () => this.toggleScale()),
             new DropDownDataType("Wings Template", "template", {
                 defaultValue: "normal",
                 values: [{ name: "Normal", value: "normal" },
@@ -31,10 +31,12 @@ export class WingsComponent extends Component {
                 values: [{ name: "Default", value: "default" }, { name: "Custom", value: "custom" }]
             }, () => this.toggleCustomWings()),
             new FileDataType("Wings Image", "wingsImage"),
+            new CheckboxDataType("Auto Resize Primary Wings", "autoSizeWingsA", true, () => this.toggleWingsAScale()),
             new DropDownDataType("Primary Wings Example", "wingsAExample", {
                 defaultValue: "example1",
                 values: [{ name: "Example 1", value: "example1" }, { name: "Example 2", value: "example2" }]
             }, null, false),
+            new CheckboxDataType("Auto Resize Secondary Wings", "autoSizeWingsB", true, () => this.toggleWingsBScale()),
             new DropDownDataType("Seconday Wings Example", "wingsBExample", {
                 defaultValue: "example1",
                 values: [{ name: "Example 1", value: "example1" }, { name: "Example 2", value: "example2" }]
@@ -68,9 +70,9 @@ export class WingsComponent extends Component {
         const pfpX = this.values.followPfp ? pfpComponent.subComponents[0].values.positionX : this.subComponents[0].values.positionX;
         const pfpY = this.values.followPfp ? pfpComponent.subComponents[0].values.positionY : this.subComponents[0].values.positionY;
 
-        const drawWings = (wingsImage, templateImage, globalCompositeOperation) => {
-            const wingsWidth = (wingsImage.width * (this.values.autoSize ? pfpComponent.values.size : this.subComponents[1].values.scaleX));
-            const wingsHeight = (wingsImage.height * (this.values.autoSize ? pfpComponent.values.size : this.subComponents[1].values.scaleY));
+        const drawWings = (wingsImage, templateImage, autoSize, scaleX, scaleY, globalCompositeOperation) => {
+            const wingsWidth = (wingsImage.width * (autoSize ? pfpComponent.values.size : scaleX));
+            const wingsHeight = (wingsImage.height * (autoSize ? pfpComponent.values.size : scaleY));
 
             const wingsX = pfpX - wingsWidth / 2.;
             const wingsY = pfpY - wingsHeight / 2.;
@@ -85,20 +87,21 @@ export class WingsComponent extends Component {
 
         if (wingsImages.length >= 2) {
             if (wingsImages[0]) {
-                drawWings(wingsImages[0], wingsTemplateImage, "source-in");
+                drawWings(wingsImages[0], wingsTemplateImage, this.values.autoSizeWingsA, this.subComponents[1].values.wingsAScaleX, this.subComponents[1].values.wingsAScaleY, "source-in");
             }
 
             if (wingsImages[1]) {
-                drawWings(wingsImages[1], wingsTemplateImage, "source-out");
+                drawWings(wingsImages[1], wingsTemplateImage, this.values.autoSizeWingsB, this.subComponents[2].values.wingsBScaleX, this.subComponents[2].values.wingsBScaleY, "source-out");
             }
         } else if (wingsImages[0]) {
-            drawWings(wingsImages[0], null, "source-in");
+            drawWings(wingsImages[0], null, this.values.autoSizeWingsA, this.subComponents[1].values.wingsAScaleX, this.subComponents[1].values.wingsAScaleY, "source-in");
         }
     }
 
     onLoad() {
         this.togglePosition();
-        this.toggleScale();
+        this.toggleWingsAScale();
+        this.toggleWingsBScale();
         this.toggleCustomWings();
     }
 
@@ -110,23 +113,35 @@ export class WingsComponent extends Component {
         }
     }
 
-    toggleScale() {
-        if (this.values.autoSize) {
+    toggleWingsAScale() {
+        if (this.values.autoSizeWingsA) {
             this.subComponents[1].hide();
         } else {
             this.subComponents[1].show();
         }
     }
 
+    toggleWingsBScale() {
+        if (this.values.autoSizeWingsB) {
+            this.subComponents[2].hide();
+        } else {
+            this.subComponents[2].show();
+        }
+    }
+
     toggleCustomWings() {
         if (this.values.type == "custom") {
-            this.dataTypes[4].show();
+            this.dataTypes[3].show();
+            this.dataTypes[4].hide();
             this.dataTypes[5].hide();
             this.dataTypes[6].hide();
+            this.dataTypes[7].hide();
         } else {
-            this.dataTypes[4].hide();
+            this.dataTypes[3].hide();
+            this.dataTypes[4].show();
             this.dataTypes[5].show();
             this.dataTypes[6].show();
+            this.dataTypes[7].show();
         }
     }
 }
--- scripts/subComponents/scaleComponent.mjs
+++ scripts/subComponents/scaleComponent.mjs
@@ -2,15 +2,16 @@ import { SubComponent } from "../subComponent.mjs";
 import { SliderDataType } from "../dataTypes/sliderDataType.mjs";
 import { ButtonDataType } from "../dataTypes/buttonDataType.mjs";
 
-export class ScaleComponent extends SubComponent{
-    constructor() {
-        super("Scale");
+export class ScaleComponent extends SubComponent {
+    constructor(customName = "Scale", customId = "scale") {
+        super(customName);
+        this.customId = customId;
     }
 
-    createDataTypes(){
-        this.dataTypes=[
-            new SliderDataType("Scale X", "scaleX", 1, 0.5, 1.5, 0.1),
-            new SliderDataType("Scale Y", "scaleY", 1, 0.5, 1.5, 0.1),
+    createDataTypes() {
+        this.dataTypes = [
+            new SliderDataType(`${this.name} X`, `${this.customId}X`, 1, 0.5, 1.5, 0.1),
+            new SliderDataType(`${this.name} Y`, `${this.customId}Y`, 1, 0.5, 1.5, 0.1),
             new ButtonDataType("Reset to default", `resetButton`, () => {
                 this.dataTypes[0].value = 1;
                 this.dataTypes[1].value = 1;
